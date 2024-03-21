c8878ea7af3a76a17a38f14de2509d58b5968a17
kangtaeui <141004174+kangtaeui@users.noreply.github.com>
2024-03-21 17:57:31 +0900

    Update main.c

--- main.c
+++ main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "LCD.h"
+#include "SystemConfig.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -62,40 +63,13 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-static char ALWOX[]  = "RCC FLASH ADDED";
-static char MYNAME[] = "SET WITHOUT MX";
+static char ALWOX[]  = "ALWOX";
+static char MYNAME[] = "TAEUI";
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-void SystemClock_Init()
-{
-
-  *((uint32_t *) RCC_CR_SET) |= 0x03037b83;
-  *((uint32_t *) RCC_PLLCFGR_SET) |= 0x22412d04;        // SET PLLR, PLLQ,  PLLSRT, PLLP, PLLN, PLLM
-  *((uint32_t *) RCC_CFGR_SET) |= 0x0000140f;           // SET AHB clock divided by 4. PLL_R USED, SELECTED 
-  *((uint32_t *) RCC_CSR_SET) |= 0x1e000000;            // SET SOFTWARE RESET, (PORT, PIN, BOR) RESET FLAG
-  
-  *((uint32_t *) RCC_PLLI2SCFGR_SET) |= 0x24002D10;    // SET PRESCALE VALUE and Output 180Hz frequency
-  *((uint32_t *) RCC_PLLSAICFGR_SET) |= 0x04002D10;     //SET PRESCALE VALUE and Output 180Hz frequency
-  //AxB1-------------------------------------------------------------------------------
-  *((uint32_t *) RCC_AHB1ENR_SET) |=0x00000017;         // SET GPIO A,B,C,H H>>OSCILATOR ENABLE
-  *((uint32_t *) RCC_AHB1LPENR_SET) |= 0x60079007;      // SET GPIO FLASH interface clock, GPIO A~C; DURING Sleep mode; 
-  
-  *((uint32_t *) RCC_APB1ENR_SET) |= 0x10000000;        // SET pwr clock enable
-  *((uint32_t *) RCC_APB1LPENR_SET) |= 0x18000000;      // set PWR, CECLP ENABLE
-  //AxB2-------------------------------------------------------------------------------
-  *((uint32_t *) RCC_APB2ENR_SET) |= 0x00004000;        // SET system clock configuration enable
-  *((uint32_t *) RCC_APB2LPENR_SET) |= 0x00004000;      // set systemconfig controller clock enable during sleep mode
- 
-  //AxB3-------------------------------------------------------------------------------
-  *((uint32_t *) RCC_AHB3ENR_SET) |= 0x00000003;        // SET memory controller module clock, Flexible memory controller module clock enable
-  
-  //Flash set
-  *((uint32_t *) FLASH_ACR_SET) |= 0x00000705;          //set 5 Latency, cache reset, data cach reset func==0
-  
-} 
 
 /* USER CODE END PFP */
 
@@ -127,6 +101,7 @@ int main(void)
   //SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
+  //SystemClock_Init();
   SystemClock_Init();
   /* USER CODE END SysInit */
 
@@ -135,19 +110,7 @@ int main(void)
   /* USER CODE BEGIN 2 */
   LcdInit();
   //SET D0~D7 MODER
-  GPIOA->MODER |= ( 1 << 0 );	// SET PA0 OUTPUT MODE
-  GPIOA->MODER |= ( 1 << 2 );	// SET PA1 OUTPUT MODE
-  GPIOA->MODER |= ( 1 << 8 );	// SET PA4 OUTPUT MODE
-  GPIOA->MODER |= ( 1 << 12 );  // SET PA6 OUTPUT MODE
-  GPIOA->MODER |= ( 1 << 14 );	// SET PA7 OUTPUT MODE
-  GPIOA->MODER |= ( 1 << 16 );	// SET PA8 OUTPUT MODE
-  GPIOA->MODER |= ( 1 << 18 );	// SET PA9 OUTPUT MODE
-  GPIOA->MODER |= ( 1 << 20 );	// SET PA10 OUTPUT MODE
-
-  //SET RW,RS,E MODER
-  GPIOB->MODER |= ( 1 << 8 );	//SET PB4 OUTPUT MODE
-  GPIOB->MODER |= ( 1 << 10 );	//SET PB5 OUTPUT MODE
-  GPIOB->MODER |= ( 1 << 12 );	//SET PB6 OUTPUT MODE
+  GPIO_MODER();
   
   //SET BUTTON PIN MODER
   //LCD_BUTTON_PORT -> MODER |= ( 1 << 26 );	//SET PC13 OUTPUT MODE
